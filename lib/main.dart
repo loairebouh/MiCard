2f70f2d0fcdb37c8b926e91542b2cdb1358ed049
Loai Rebouh <loai_rebouh@icloud.com>
2024-12-20 00:10:28 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.only(left: 30),
+            color: Colors.white,
+            child: Text("Hello"),
+          ),
+        ),
+      ),
+    ); // Closing parenthesis and semicolon added.
+  }
+}
